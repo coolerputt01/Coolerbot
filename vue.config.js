0f5c1e3110a8f2b4acd3db02e9e74a424be6d2af
coolerputt01 <olumideadekolu9@gmail>
2025-03-07 17:24:59 -0800

    Commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'Cooler Bot',
+    themeColor: '#000',
+    manifestOptions: {
+      background_color: '#000'
+    },
+    workboxOptions: {
+      skipWaiting: true
+    }
+  }
 })
